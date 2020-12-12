b2d9497a8bf7c457067a9a83a22ef2eaf0dcad9f
hk7math <hk7math@gmail.com>
2020-12-12 21:05:36 +0800

    8.4: Books of an author

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!,
     authorCount: Int!,
-    allBooks: [Book!]!,
+    allBooks(author: String): [Book!]!,
     allAuthors: [Author!]!
   }
 `
@@ -109,7 +109,7 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => books.filter(book => !args.author || args.author === book.author),
     allAuthors: () => authors
   },
   Author: {
